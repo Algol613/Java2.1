305f587d5ed46f83a00c179b9235d49ef59fbe93
Svetlana Bodnar <ss.bodnar@yandex.ru>
2020-03-23 11:41:01 +0300

    first commit

---
+++ src/BankTransfer.java
@@ -0,0 +1,11 @@
+public class BankTransfer {
+
+    public static void main(String[] args) {
+        int score = 2_000_000_000;
+        int transfer = 500_000_000;
+        score = score + transfer;
+
+        System.out.println(score);
+    }
+
+}
